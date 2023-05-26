7423cb5f919ac3aba605b7c2291054f2487d340f
Gleb <M20XV@yandex.ru>
2023-05-26 15:04:24 +0300

    fix

--- src/downloadSkin.js
+++ src/downloadSkin.js
@@ -1,12 +1,29 @@
 const sharp = require('sharp')
 const imgCache = require('./imgCache')
+const cache = require('./cache')
 const api = require('./api')
+
+function getUUID(username) {
+  return cache(`uuid-${username}`, () => api.getUserId(username))
+}
+
+function getUrl(username) {
+  return cache(`url-${username}`, async () => {
+    const uuid = await getUUID(username)
+    return api.getSkinUrl(username)
+  })
+}
+
+function getSourceBuffer(username) {
+  return imgCache(`source-${username}.png`, async () => {
+    const url = await getUrl(username)
+    return api.getImage(url)
+  })
+}
 
 module.exports = async function(username) {
   const head = await imgCache(`head-${username}.png`, async () => {
-    const sourceBuffer = await imgCache(`source-${username}.png`, async () => {
-      return 
-    })
+    const sourceBuffer = await getSourceBuffer()
 
     return sharp(sourceBuffer).extract({ 
       left: 8, 
