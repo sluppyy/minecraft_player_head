fcf372c90491c6a7dcdf24d198a5803ce982c2bc
Gleb <M20XV@yandex.ru>
2023-05-26 16:05:33 +0300

    add dockerfile

--- src/index.js
+++ src/index.js
@@ -1,34 +1,34 @@
-const { env } = require('./env')
-const downloadSkin = require('./downloadSkin')
-const logger = require('./logger')
-const fs = require('fs/promises')
-const { AxiosError } = require('axios')
+const { env } = require("./env");
+const downloadSkin = require("./downloadSkin");
+const logger = require("./logger");
+const { AxiosError } = require("axios");
 
-const app = require('express')()
+const app = require("express")();
 
-app.get('/:username', async (req, res) => {
-  const username = req.params.username
+app.get("/:username", async (req, res) => {
+  const username = req.params.username;
   try {
-    const buffer = await downloadSkin(username)
+    const buffer = await downloadSkin(username);
     res.writeHead(200, {
-      'Content-Type': 'image/png',
-      'Content-Length': buffer.length
-    })
-    res.end(buffer)  
+      "Content-Type": "image/png",
+      "Content-Length": buffer.length,
+    });
+    res.end(buffer);
   } catch (error) {
     if (error instanceof AxiosError) {
       if (error.response.status === 404) {
-        res.sendStatus(404)
-        return
+        res.sendStatus(404);
+        return;
       }
     }
 
-    logger.error(error)
-    res.status(500)
-    res.send('internal error')
+    logger.error(error);
+    res.status(500);
+    res.send("internal error");
   }
-})
+});
 
-app.listen(env['PORT'], () => {
-  console.log(`App start on port ${env['PORT']}`)
-})
+const port = env["PORT"] ?? 3000;
+app.listen(port, () => {
+  console.log(`App start on port ${port}`);
+});
